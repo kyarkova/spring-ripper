c86e9f05f2c7340efd3161c70ff62a2ef60d5436
Kseniia Iarkova <kseniia_iarkova@epam.com>
2017-12-01 02:29:24 +0300

    add custom scope example

---
+++ JavaLab/ripper/src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,30 @@
+package screensaver;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Service
+@Scope("prototype")
+public abstract class ColorFrame extends JFrame {
+
+    public ColorFrame() throws HeadlessException {
+        setSize(150, 150);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+        getContentPane().setBackground(getColor());
+        repaint();
+    }
+
+    protected abstract Color getColor();
+}
+
+
