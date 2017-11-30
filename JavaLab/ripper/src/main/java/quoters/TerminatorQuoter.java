1f7909ce7dff82ee1c48d166dbc4b66ce050c487
Kseniia Iarkova <kseniia_iarkova@epam.com>
2017-12-01 01:19:47 +0300

    add new context example

--- JavaLab/ripper/src/main/java/quoters/TerminatorQuoter.java
+++ JavaLab/ripper/src/main/java/quoters/TerminatorQuoter.java
@@ -4,11 +4,15 @@ import javax.annotation.PostConstruct;
 @Profiling
 @DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
+
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
     private String message;
 
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
+    }
     @PostConstruct
     public void init() {
         System.out.println("Phase 2");
